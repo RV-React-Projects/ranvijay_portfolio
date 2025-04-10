1c190f7342ca226b00f0a22d916bad56c68d8780
Ranvijay <ranvijaychouhan12@gmail.com>
2025-04-10 20:20:20 +0530

    Initial Commit + Added ShadCN componnets

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,61 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'import/no-unresolved': [2, { caseSensitive: false }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'import/order': [
+        'warn',
+        {
+          groups: [['builtin'], 'external', 'internal', ['parent', 'sibling']],
+          pathGroups: [
+            {
+              pattern:
+                '@{redux,themes,assets,components,conf,hooks,navigation,storage,thunk,slice,screens,themes,colors,utils,models,app,network,common,services,context,constants}/**',
+              group: 'internal',
+              position: 'before',
+            },
+            {
+              pattern: '@**',
+              group: 'external',
+              position: 'after',
+            },
+            {
+              pattern: 'react**',
+              group: 'builtin',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'never',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'react/jsx-sort-props': [
+        'warn',
+        {
+          callbacksLast: true,
+          shorthandFirst: true,
+          noSortAlphabetically: false,
+          reservedFirst: true,
+        },
+      ],
+      'padding-line-between-statements': [
+        'warn',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,39 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+let baseUrl;
+
+switch (process.env.NEXT_PUBLIC_STAGE?.trim()) {
+  case 'DEV':
+    baseUrl = 'https://google.com/';
+    break;
+  case 'UAT':
+    baseUrl = 'https://google.com/';
+    break;
+  case 'PROD':
+    baseUrl = 'hhttps://google.com/';
+    break;
+  default:
+    baseUrl = 'https://google.com/';
+    break;
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  crossOrigin: 'anonymous',
+  env: {
+    baseUrl,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
