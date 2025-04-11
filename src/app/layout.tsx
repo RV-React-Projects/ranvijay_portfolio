5b135921fb3c6d735fee8aaae6179d6294a053fb
Ranvijay <ranvijaychouhan12@gmail.com>
2025-04-11 17:31:35 +0530

    Starting with Building

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@styles/globals.css';
+import Footer from '@components/Footer';
+import Header from '@components/Header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,9 +27,11 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div></div>
-        {children}
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Header />
+        <div className="min-h-screen">{children}</div>
+        <Footer />
       </body>
     </html>
   );
